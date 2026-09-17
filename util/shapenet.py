02dac9a07d7a33b1a279de5c7f5c20eed3dabe05
TarekHiisham <tarekhesham2017@gmail.com>
2026-09-18 01:25:43 +0200

    modifing shapenet module

--- util/shapenet.py
+++ util/shapenet.py
@@ -73,109 +73,92 @@ category_ids = {
 }
 
 class ShapeNet(data.Dataset):
-    def __init__(self, dataset_folder, split, categories=None, transform=None, sampling=True, num_samples=4096, return_surface=True, surface_sampling=True, pc_size=2048, replica=16):
+    def __init__(
+        self, 
+        dataset_folder, 
+        split='train', 
+        category_id='03001627',
+        num_query_pts=2048, 
+        pc_size=2048, 
+        return_surface=True, 
+        transform=None, 
+        train_ratio=0.8, 
+        val_ratio=0.1,
+        seed=42
+    ):
         
-        self.pc_size = pc_size
-
-        self.transform = transform
-        self.num_samples = num_samples
-        self.sampling = sampling
+        self.surfaces_dir = os.path.join(dataset_folder, category_id, '4_pointcloud')
+        self.occupancies_dir = os.path.join(dataset_folder, 'ShapeNetV2_point', category_id)
+        
         self.split = split
-
-        self.dataset_folder = dataset_folder
+        self.num_query_pts = num_query_pts
+        self.pc_size = pc_size
         self.return_surface = return_surface
-        self.surface_sampling = surface_sampling
+        self.transform = transform
 
-        self.dataset_folder = dataset_folder
-        self.point_folder = os.path.join(self.dataset_folder, 'ShapeNetV2_point')
-        self.mesh_folder = os.path.join(self.dataset_folder, 'ShapeNetV2_watertight')
+        surf_models = set(os.listdir(self.surfaces_dir))
+        occ_models = set(os.listdir(self.occupancies_dir))
+        common_models = sorted(list(surf_models.intersection(occ_models)))
 
-        if categories is None:
-            categories = os.listdir(self.point_folder)
-            categories = [c for c in categories if os.path.isdir(os.path.join(self.point_folder, c)) and c.startswith('0')]
-        categories.sort()
-
-        print(categories)
-
-        self.models = []
-        for c_idx, c in enumerate(categories):
-            subpath = os.path.join(self.point_folder, c)
-            assert os.path.isdir(subpath)
-
-            split_file = os.path.join(subpath, split + '.lst')
-            with open(split_file, 'r') as f:
-                models_c = f.read().split('\n')
-            
-            self.models += [
-                {'category': c, 'model': m.replace('.npz', '')}
-                for m in models_c
-            ]
-
-        self.replica = replica
-
-    def __getitem__(self, idx):
-        idx = idx % len(self.models)
-
-        category = self.models[idx]['category']
-        model = self.models[idx]['model']
+        rng = np.random.default_rng(seed)
+        shuffled_indices = rng.permutation(len(common_models))
         
-        point_path = os.path.join(self.point_folder, category, model+'.npz')
-        try:
-            with np.load(point_path) as data:
-                vol_points = data['vol_points']
-                vol_label = data['vol_label']
-                near_points = data['near_points']
-                near_label = data['near_label']
-        except Exception as e:
-            print(e)
-            print(point_path)
+        n_train = int(train_ratio * len(common_models))
+        n_val = int(val_ratio * len(common_models))
 
-        with open(point_path.replace('.npz', '.npy'), 'rb') as f:
-            scale = np.load(f).item()
-
-        if self.return_surface:
-            pc_path = os.path.join(self.mesh_folder, category, '4_pointcloud', model+'.npz')
-            with np.load(pc_path) as data:
-                surface = data['points'].astype(np.float32)
-                surface = surface * scale
-            if self.surface_sampling:
-                ind = np.random.default_rng().choice(surface.shape[0], self.pc_size, replace=False)
-                surface = surface[ind]
-            surface = torch.from_numpy(surface)
-
-        if self.sampling:
-            ind = np.random.default_rng().choice(vol_points.shape[0], self.num_samples, replace=False)
-            vol_points = vol_points[ind]
-            vol_label = vol_label[ind]
-
-            ind = np.random.default_rng().choice(near_points.shape[0], self.num_samples, replace=False)
-            near_points = near_points[ind]
-            near_label = near_label[ind]
-
-        
-        vol_points = torch.from_numpy(vol_points)
-        vol_label = torch.from_numpy(vol_label).float()
-
-        if self.split == 'train':
-            near_points = torch.from_numpy(near_points)
-            near_label = torch.from_numpy(near_label).float()
-
-            points = torch.cat([vol_points, near_points], dim=0)
-            labels = torch.cat([vol_label, near_label], dim=0)
+        if split == 'train':
+            selected_idx = shuffled_indices[:n_train]
+        elif split == 'val':
+            selected_idx = shuffled_indices[n_train:n_train + n_val]
+        elif split == 'test':
+            selected_idx = shuffled_indices[n_train + n_val:]
         else:
-            points = vol_points
-            labels = vol_label
+            raise ValueError(f"Unknown split: {split}. Choose from ['train', 'val', 'test']")
 
-        if self.transform:
-            surface, points = self.transform(surface, points)
-
-        if self.return_surface:
-            return points, labels, surface, category_ids[category]
-        else:
-            return points, labels, category_ids[category]
+        self.models = [common_models[i] for i in selected_idx]
 
     def __len__(self):
-        if self.split != 'train':
-            return len(self.models)
+        return len(self.models)
+
+    def __getitem__(self, idx):
+        model_id = self.models[idx]
+
+        occ_file = os.path.join(self.occupancies_dir, model_id, '.npz')
+        with np.load(occ_file) as occ_data:
+            query_points = occ_data['points'].astype(np.float32)
+            raw_labels = occ_data['occupancies']
+
+        if raw_labels.dtype == np.uint8:
+            labels = np.unpackbits(raw_labels)[:query_points.shape[0]].astype(np.float32)
         else:
-            return len(self.models) * self.replica
+            labels = raw_labels.astype(np.float32)
+
+        if self.num_query_pts and len(query_points) >= self.num_query_pts:
+            ind_occ = np.random.choice(len(query_points), self.num_query_pts, replace=False)
+            query_points = query_points[ind_occ]
+            labels = labels[ind_occ]
+
+        query_points = torch.from_numpy(query_points)
+        labels = torch.from_numpy(labels)
+
+        surface = None
+        if self.return_surface:
+            surf_file = os.path.join(self.surfaces_dir, model_id, '.npz')
+            with np.load(surf_file) as surf_data:
+                surf_pts = surf_data['points'].astype(np.float32)
+
+            if self.pc_size and len(surf_pts) >= self.pc_size:
+                ind_surf = np.random.choice(len(surf_pts), self.pc_size, replace=False)
+                surf_pts = surf_pts[ind_surf]
+
+            surface = torch.from_numpy(surf_pts)
+
+        if self.transform:
+            if self.return_surface:
+                surface, query_points = self.transform(surface, query_points)
+            else:
+                query_points = self.transform(query_points)
+
+        if self.return_surface:
+            return query_points, labels, surface
+        return query_points, labels
