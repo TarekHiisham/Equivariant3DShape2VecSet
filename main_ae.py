215cfb69a8103ea37e57891dbadad529ab1cdc40
TarekHiisham <tarekhesham2017@gmail.com>
2026-09-19 16:38:09 +0200

    changing epoch size

--- main_ae.py
+++ main_ae.py
@@ -23,9 +23,9 @@ from engine_eqae import train_one_epoch, evaluate
 
 def get_args_parser():
     parser = argparse.ArgumentParser('Autoencoder', add_help=False)
-    parser.add_argument('--batch_size', default=64, type=int,
+    parser.add_argument('--batch_size', default=16, type=int,
                         help='Batch size per GPU (effective batch size is batch_size * accum_iter * # gpus')
-    parser.add_argument('--epochs', default=800, type=int)
+    parser.add_argument('--epochs', default=100, type=int)
     parser.add_argument('--accum_iter', default=1, type=int,
                         help='Accumulate gradient iterations (for increasing the effective batch size under memory constraints)')
 
