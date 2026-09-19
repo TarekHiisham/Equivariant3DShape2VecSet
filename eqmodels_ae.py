082d114f8c15d6f9a488ef5062f6fb905c7aa994
TarekHiisham <tarekhesham2017@gmail.com>
2026-09-20 00:30:37 +0200

    samples change

--- eqmodels_ae.py
+++ eqmodels_ae.py
@@ -275,7 +275,7 @@ def create_autoencoder(irreps_dim="32x0e + 16x1o", M=512, N=2048, determinisitc=
 def ae_d512_m512(N=2048):
     return create_autoencoder(irreps_dim="32x0e + 16x1o", M=512, N=N, determinisitc=True)
 
-def ae_d512_m256(N=2048):
+def ae_d512_m256(N=1024):
     return create_autoencoder(irreps_dim="32x0e + 16x1o", M=256, N=N, determinisitc=True)
 
 def ae_d512_m128(N=2048):
