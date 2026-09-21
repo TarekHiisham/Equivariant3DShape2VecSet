9525bdd70338474d0e2087b448c3f69f3e287f90
TarekHiisham <tarekhesham2017@gmail.com>
2026-09-22 01:48:26 +0200

    reduced model

--- eqmodels_ae.py
+++ eqmodels_ae.py
@@ -202,10 +202,10 @@ class EquivariantAutoEncoder(nn.Module):
     def __init__(
         self,
         *,
-        depth=6,
-        irreps_dim="128x0e + 64x1o",
-        num_inputs = 2048,
-        num_latents = 256,
+        depth=5,
+        irreps_dim="64x0e + 64x1o",
+        num_inputs = 128,
+        num_latents = 128,
     ):
         super().__init__()
 
@@ -343,5 +343,5 @@ def ae_d64_m512(N=2048):
     return create_autoencoder(irreps_dim="32x0e + 32x1o", M=512, N=N, determinisitc=True)
 
 ### Reduced version 
-def ae_d320_m256(N=128):
+def ae_d256_m128(N=128):
     return create_autoencoder(irreps_dim="64x0e + 64x1o", M=128, N=N, determinisitc=True)
--- main_ae.py
+++ main_ae.py
@@ -30,7 +30,7 @@ def get_args_parser():
                         help='Accumulate gradient iterations (for increasing the effective batch size under memory constraints)')
 
     # Model parameters
-    parser.add_argument('--model', default='ae_d320_m256', type=str, metavar='MODEL',
+    parser.add_argument('--model', default='ae_d256_m128', type=str, metavar='MODEL',
                         help='Name of model to train')
 
     parser.add_argument('--point_cloud_size', default=128, type=int,
@@ -79,7 +79,7 @@ def get_args_parser():
                         help='Perform evaluation only')
     parser.add_argument('--dist_eval', action='store_true', default=False,
                         help='Enabling distributed evaluation (recommended during training for faster monitor')
-    parser.add_argument('--num_workers', default=8, type=int)
+    parser.add_argument('--num_workers', default=2, type=int)
     parser.add_argument('--pin_mem', action='store_true',
                         help='Pin CPU memory in DataLoader for more efficient (sometimes) transfer to GPU.')
     parser.add_argument('--no_pin_mem', action='store_false', dest='pin_mem')
